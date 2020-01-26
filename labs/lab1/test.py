b926f2d85456241ec97d35b6622f9b0f221c8761
John L. Whiteman <>
2020-01-26 09:31:17 -0800

    Added test script for lab 1
    Signed-off-by: John L. Whiteman <>

--- labs/lab1/test.py
+++ labs/lab1/test.py
@@ -1,28 +1,105 @@
 import json
+import os
+import subprocess
 import sys
 from  pprint import pprint
 
+class Result:
+    def __init__(self):
+        self.ret = None
+        self.out = None
+        self.err = None
+        self.cmd = None
+
+class Test:
+    def __init__(self, test):
+        self.c = test["c"]
+        self.k = test["k"]
+        self.p = test["p"]
+        self.kpath = "k.txt"
+        self.cpath = "c.txt"
+        self.ppath = "p.txt"
+
+def read(path):
+    try:
+        with open(path, "r",  encoding="utf-8") as fd:
+            content = fd.read()
+            return content.strip()
+    except IOError as e:
+        print("Abort: {0}".format(e))
+        sys.exit(1)
+
+def run(args):
+    r = Result()
+    r.cmd = "./vigenere {0}".format(args)
+    p = subprocess.Popen(r.cmd,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+        shell=True)
+    stdout, stderr = p.communicate()
+    r.out = stdout.decode("utf8").strip()
+    r.err = stderr.decode("utf8").strip()
+    r.ret = p.returncode
+    if r.ret:
+        print("Excution Failed: {0}".format(t.cmd))
+        sys.exit(1)
+    return r
+
 def testEncryption(t):
-    pprint(t)
+    r = run("-e -k '{0}' -i '{1}'".format(t.k, t.p))
+    if r.out != t.c:
+        print("Encryption Failed: {0}".format(r.cmd))
+        print("{0} != {1}".format(r.out, t.c))
+        sys.exit(1)
+    print("Encryption Passed")
 
 def testDecryption(t):
-    pprint(t)
+    r = run("-d -k '{0}' -i '{1}'".format(t.k, t.p))
+    if r.out != t.p:
+        print("Decryption Failed: {0}".format(r.cmd))
+        print("{0} != {1}".format(r.out, t.p))
+        sys.exit(1)
+    print("Decryption Passed")
 
 def testEncryptionFile(t):
-    pprint(t)
+    r = run("-E -k '{0}' -i '{1}' -o '{2}'".format(
+            t.kpath, t.ppath, t.cpath))
+    if not os.path.exists(t.cpath):
+        print("Encryption File Failed: No {0}".format(
+            t.cpath))
+        sys.exit(1)
+    c = read(t.cpath)
+    if r.out != c:
+        print("Encryption File Failed: {0}".format(r.cmd))
+        print("{0} != {1}".format(r.out, t.c))
+        sys.exit(1)
+    print("Encryption File Passed")
 
 def testDecryptionFile(t):
-    pprint(t)
+    r = run("-D -k '{0}' -i '{1}' -o '{2}'".format(
+            t.kpath, t.cpath, t.ppath))
+    if not os.path.exists(t.ppath):
+        print("Decryption File Failed: No {0}".format(
+            t.ppath))
+        sys.exit(1)
+    p = read(t.ppath)
+    if r.out != p:
+        print("Decryption File Failed: {0}".format(r.cmd))
+        print("{0} != {1}".format(r.out, t.p))
+        sys.exit(1)
+    print("Decryption File Passed")
 
-try:
-    with open("tests.json", "r", encoding="utf-8") as fd:
-        tests = json.load(fd)
-        for t in tests:
-            testEncryption(t)
-            testDecryption(t)
-            testEncryptionFile(t)
-            testDecryptionFile(t)
-
-except IOError as e:
-    print("Why!")
-    sys.exit(1)
+if __name__ == "__main__":
+    try:
+        with open("tests.json", "r",
+                  encoding="utf-8") as fd:
+            tests = json.load(fd)
+            for test in tests:
+                t = Test(test)
+                testEncryption(t)
+                testDecryption(t)
+                testEncryptionFile(t)
+                testDecryptionFile(t)
+    except IOError as e:
+        print("Abort: {0}".format(e))
+        sys.exit(1)
--- labs/lab1/vigenere.c
+++ labs/lab1/vigenere.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
 
+int decrypt(char *k, char *c, char *result);
+int encrypt(char *k, char *p, char *result);
 void showUsage(void);
 
 int main(int argc, char **argv) {
-
-    /* Your code goes here */
-
-    /* comment out */
+    /* Add your code here */
     showUsage();
-
     return 0;
+}
+
+int decrypt(char *k, char *c, char *result) {
+    /* Add your code here */
+    return 1;
+}
+
+int encrypt(char *k, char *p, char *result) {
+    /* Add your code here */
+    return 1;
 }
 
 void showUsage() {
@@ -17,18 +26,12 @@ void showUsage() {
     printf("\nVigenère is a simple substitution cipher program\n");
 
     printf("\nUse Case: Argument-Based\
-          \n\nE(k,p): vigenere -e -k 'k' -i 'p'\
-          \nD(k,c): vigenere -d -k 'k' -i 'p'\n\n");
+\n\nE(k,p): vigenere -e -k 'ktext' -i 'ptext'\
+\nD(k,c): vigenere -d -k 'ktext' -i 'ctext'\n");
 
-    printf("Use Case: File-Based\
-          \n\nE(k,p): vigenere -E -k 'kpath' -i 'ppath' -o 'cpath'\
-          \nD(k,c): vigenere -D -k 'kpath' -i 'cpath' -o 'ppath'\n\n");
+    printf("\nUse Case: File-Based\
+\n\nE(k,p): vigenere -E -k 'kpath' -i 'ppath' -o 'cpath'\
+\nD(k,c): vigenere -D -k 'kpath' -i 'cpath' -o 'ppath'\n\n");
 
-    printf("k: key\
-        \np: plaintext\
-        \nc: ciphertext\
-        \nE(k,p): Encrypt\
-        \nD(k,c): Decrypt\
-        \ni: input or path to input file\
-        \no: path to output file\n\n");
 }
+
