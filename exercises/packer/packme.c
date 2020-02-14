4f87812c6824c44374da308ccf88af5f34b3a044
John L. Whiteman <>
2020-02-14 18:03:45 -0500

    packer exercise packed full of fun
    Signed-off-by: John L. Whiteman <>

---
+++ exercises/packer/packme.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char *getPasswrd() {
+    char *pwd = "X@=Nothing2Chere!";
+    return pwd;
+}
+
+int foo1() {
+    int a, b, c = 1;
+    a = a * b + c;
+    return a;
+}
+
+int main() {
+    printf("I work.\n");
+    return 0;
+}
